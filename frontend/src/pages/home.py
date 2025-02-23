dbdb8dac24d2344e2252a0b3795b776ac7604382
josuemj <98498772+josuemj@users.noreply.github.com>
2025-02-22 23:16:51 -0600

    fix -> deprecated .image argument and image src :)

--- frontend/src/pages/home.py
+++ frontend/src/pages/home.py
@@ -3,4 +3,4 @@ import streamlit as st
 def show():
     st.title("🎬 Entertainment Recommendation")
     st.write("Bienvenido al sistema de recomendación de entretenimiento.")
-    st.image("https://source.unsplash.com/800x400/?movies,cinema", use_container_width=True)
+    st.image("https://wallpapers.com/images/featured/film-pictures-vbq45si2ir8k7fw3.jpg")
